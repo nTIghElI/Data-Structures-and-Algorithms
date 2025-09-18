cfab6b63cf6e937ba5ec53952f94ab8f6898a69f
Snow_Crash <greer.seth@outlook.com>
2025-09-18 13:59:28 -0400

    ICommit

---
+++ Week_1/RedisCachingApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.StackExchangeRedis;
+class ProgramCashe
+{
+    static async Task Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        services.AddStackExchangeRedisCache(options =>
+        {
+            options.Configuration = "localhost:6379";
+            options.InstanceName = "SampleInstance";
+        });
+        var provider = services.BuildServiceProvider();
+        var cache = provider.GetService<IDistributedCache>();
+        string cacheKey = "SharedCounter";
+        string cachedValue = await cache.GetStringAsync(cacheKey);
+        int counter = cachedValue != null ? int.Parse(cachedValue) : 0;
+        counter++;
+        await cache.SetStringAsync(cacheKey, counter.ToString());
+        Console.WriteLine($"Updated Counter: {counter}");
+    }
+}
